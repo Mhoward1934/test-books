c478618df484574246bfa1adb3ea5e020bf678fd
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2019-03-08 15:00:42 -0600

    updated landing page

--- client/src/pages/landing/landing.js
+++ client/src/pages/landing/landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "landing.css";
 
 
@@ -7,7 +8,7 @@ const LandingPage = () => (
         <div className="hero-text">
             <h1>Welcome to Google Books Search</h1>
             <h4>Search for your favorite books and save them to read later.</h4>
-            <a href="client/src/App.js"><button>Get Started!</button></a>
+            <Link href="/" to="/"><button>Get Started!</button></Link>
         </div>
     </div>
 )
